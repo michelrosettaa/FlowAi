3f9b8ecf73213aa9a1fd56e7685b6a888dbf6d0e
Michel <michel@Mac.lan>
2025-11-09 17:49:30 +0000

    disable eslint for render deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,10 @@
+// next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
-    // Let the app build on Vercel even if ESLint has errors
     ignoreDuringBuilds: true,
   },
   typescript: {
-    // Let the app build on Vercel even if TS has type errors
     ignoreBuildErrors: true,
   },
 };
