eb8ed937f5598222550b72904c9e92fedea4f43a
michelrosettaa <214337242+michelrosettaa@users.noreply.github.com>
2025-12-16 22:41:40 +0000

    restore nextauth route

--- app/api/[...nextauth]/route.ts
+++ app/api/[...nextauth]/route.ts
@@ -1,14 +1,3 @@
-import NextAuth from "next-auth";
-import Google from "next-auth/providers/google";
-
-export const { handlers, auth } = NextAuth({
-  providers: [
-    Google({
-      clientId: process.env.AUTH_GOOGLE_ID!,
-      clientSecret: process.env.AUTH_GOOGLE_SECRET!,
-    }),
-  ],
-  secret: process.env.NEXTAUTH_SECRET,
-});
+import { handlers } from "@/lib/auth";
 
 export const { GET, POST } = handlers;
